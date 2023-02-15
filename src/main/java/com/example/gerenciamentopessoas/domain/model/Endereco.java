1293c00bac491c72bf0a475386a8e14ce237febc
Valdinar Pereira <valnarvsp@gmail.com>
2023-02-15 15:18:12 -0300

    Criação das classes do domínio Enderço e Cidade

---
+++ src/main/java/com/example/gerenciamentopessoas/domain/model/Endereco.java
@@ -0,0 +1,97 @@
+package com.example.gerenciamentopessoas.domain.model;
+
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Endereco {
+
+	@Id
+	private Long id;
+
+	@Column(name = "endereco_cep")
+	private String cep;
+
+	@Column(name = "endereco_logradouro")
+	private String logradouro;
+
+	@Column(name = "endereco_numero")
+	private String numero;
+
+	private boolean isPrincipal = false;
+
+	@ManyToOne
+	@JoinColumn(name = "endereco_cidade_id")
+	private Cidade cidade;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+
+	public String getLogradouro() {
+		return logradouro;
+	}
+
+	public void setLogradouro(String logradouro) {
+		this.logradouro = logradouro;
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+	public boolean isPrincipal() {
+		return isPrincipal;
+	}
+
+	public void setPrincipal(boolean isPrincipal) {
+		this.isPrincipal = isPrincipal;
+	}
+
+	public Cidade getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(Cidade cidade) {
+		this.cidade = cidade;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Endereco other = (Endereco) obj;
+		return Objects.equals(id, other.id);
+	}
+
+}
